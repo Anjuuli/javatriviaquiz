d7ec7073a4ba9579e2ceb47478e114dbf33f639e
Anjuli Maharjan <maharjanjuli@gmail.com>
2022-02-22 21:24:02 +0545

    java quiz

---
+++ js/questions.js
@@ -0,0 +1,131 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the return type of the hashCode() method in the Object class?",
+    answer: "int",
+    options: [
+      "Object",
+      "int",
+      "long",
+      "void"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which of the following is not a Java features?",
+    answer: "Use of pointers",
+    options: [
+      "Dynamic",
+      "Architecture neutral",
+      "Use of pointers",
+      "object-oriented"
+    ]
+  },
+  
+    {
+    numb: 3,
+    question: "Which of the following is true about the anonymous inner class?",
+    answer: "It has no class name",
+    options: [
+      "It only has methods",
+      "Objects cant be created",
+      "It has a fixed class name",
+      "it has no class name"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which package contains the Random class?",
+    answer: "java.util package",
+    options: [
+      "java.util package",
+      "java.lang package",
+      "java.awt package ",
+      "java.io package"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 6,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 7,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+  {
+    numb: 8,
+    question: "n which memory a String is stored, when we create a string using new operator?",
+    answer: "Heap memory",
+    options: [
+      "stack",
+      "String memory",
+      "Heap memory",
+      "random storage space"
+    ]
+  },
+  
+    {
+    numb: 9,
+    question: "Which of the following is an immediate subclass of the Panel class?",
+    answer: "Applet class",
+    options: [
+      "Applet class",
+      "Window class",
+      "Frame class",
+      "Dialog class"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Which of the following is a reserved keyword in Java?",
+    answer: "Strictfp",
+    options: [
+      "Strictfp",
+      "main",
+      "object ",
+      "system"
+    ]
+  },
+    
+  
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
